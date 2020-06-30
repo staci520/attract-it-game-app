522662ba071023fca86cb44d49e433f33ee9dca3
Staci Stark <staci520@yahoo.com>
2020-06-29 17:47:33 -0700

    adding live db url to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://todo_user@localhost/todos'
+  DB_URL: process.env.DB_URL || 'postgresql://todo_user@localhost/todos',
+  LIVE_DB_URL: process.env.LIVE_DB_URL || 'postgres://xdjvdiosbkdlzb:92c632986d6d071ce84bce359c4170ed05bcab8849016fcf0a86cc05440464f1@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dd7cljp2hgjsgd'
 }
