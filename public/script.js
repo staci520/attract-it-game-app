dd47a806ca7595070382c8b5d0fc852feadde919
Staci Stark <staci520@yahoo.com>
2020-07-30 17:59:54 -0700

    template modules cards full stack functionality

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 'use strict';
+const apiURL = "http://localhost:8000/api"
 
 // Code by Webdevtrick ( https://webdevtrick.com )
 $('.tabs .tab').click(function () {
@@ -38,25 +39,26 @@ function sectionDisplay() {
 function getTemplateModulesDataFromApi(queryTarget) {
 
     //Step 2a - create the url
-    const url = `https://dog.ceo/api/breed/${queryTarget}/images/random`;
+    const url = `${apiURL}/template-modules`;
     console.log(url);
     // Step 2b - make the api call using the URL, dataType (JSON or JSONP), type (GET or POST)
     fetch(url)
 
-    //Step 2c - success scenario (call the function to display the results)
-    .then(response => {
-            if (response.ok) {
-                return response.json();
+        //Step 2c - success scenario (call the function to display the results)
+        .then(responseBinary => {
+            if (responseBinary.ok) {
+                return responseBinary.json();
             }
             // DISPLAY ERRORS if the server connection works but the json data is broken
-            throw new Error(response.statusText);
+            throw new Error(responseBinary.statusText);
         })
         .then(responseJson => displayTemplateModulesSearchData(responseJson))
+        // .then(responseJson => console.log(responseJson))
 
-    // Step 2d - failure scenario  (DISPLAY ERRORS if the server connection fails)
-    .catch(err => {
-        console.log(err);
-    });
+        // Step 2d - failure scenario  (DISPLAY ERRORS if the server connection fails)
+        .catch(err => {
+            console.log(err);
+        });
 };
 
 
@@ -79,27 +81,42 @@ function displayTemplateModulesSearchData(responseJson) {
 
         //Step 3d - populate the htmlOutut variable with all the relevant data
         for (let i = 0; i < responseJson.length; i++) {
-            htmlOutput +=`
-                <h3> ${responseJson[i].name}</h3><br />
-                <p>- ${responseJson[i].population}</p>
-                <p>- ${responseJson[i].capital}</p>
-                <p>- ${responseJson[i].region}</p>
+            htmlOutput += `
+                    <div class="card">
+                        <div class="card-header row" id="heading${responseJson[i].id}" style="background-color: #${responseJson[i].title_color}"> 
+                            <div class="mb-0 col-sm-3 col-md-6">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${responseJson[i].id}"
+                                    aria-expanded="false" aria-controls="collapse${responseJson[i].id}">
+                                    <h3>${responseJson[i].id}: ${responseJson[i].title}</h3>
+                                </button>
+                            </div>
+                            <div class="col-sm-3 col-md-6 complete-status">
+                                <p><i class="fas fa-edit"></i></p>
+                            </div>
+                        </div>
+                        <div id="collapse${responseJson[i].id}" class="collapse" aria-labelledby="heading${responseJson[i].id}" data-parent="#accordion">
+                            <div class="card-body">
+                                <p class="card-text">${responseJson[i].description}</p>
+                                <form class="game-form">
+                                    <div class="form-group shadow-textarea">
+                                        <label for="FormControlTextarea"></label>
+                                        <textarea class="lg-textarea form-control z-depth-1" id="FormControlTextarea" rows="12"
+                                            placeholder="Insights..."></textarea>
+                                    </div>
+                                    <div class="button-container">
+                                        <input type="submit" class="btn btn-info" value="Add Session">
+                                    </div>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
             `;
         }
 
         //Step 3e - send the content of HTML results variable to the HTML
-        $('.js-search-results').html(htmlOutput);
+        $('#accordion').html(htmlOutput);
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 /*--- Step 3 - Using functions ---*/
@@ -112,7 +129,7 @@ $(document).ready(function () {
     // $('#get-started-section').hide();
     // $('#dashboard-section').hide();
     // $('#play-game-section').hide();
-
+    getTemplateModulesDataFromApi()
 
     //form trigger - login
     $('.login-form').submit(function (event) {
